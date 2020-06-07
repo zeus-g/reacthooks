4a0acd53497041f3e649e733e7e083cce61b685d
zeus-g <cmobil3z@gmail.com>
2020-06-07 17:22:13 +0700

    created useState hooks React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [people, setPeople] = React.useState([
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Prilly",
+      lastName: "Latuconsina",
+    },
+  ]);
+  const [person, setPerson] = React.useState({ firstName: "", lastName: "" });
+  const onChange = (e) => {
+    setPerson({ ...person, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName.trim() === "" || person.lastName.trim() === "") return;
+
+    const newPerson = {
+      firstName: person.firstName.trim(),
+      lastName: person.lastName.trim(),
+    };
+
+    setPeople([...people, newPerson]);
+    setPerson({ firstName: "", lastName: "" });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>Add a Person :</h2>
+
+          <hr />
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="firstName"
+                placeholder="First Name..."
+                value={person.firstName}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="lastName"
+                placeholder="Last Name..."
+                value={person.lastName}
+                onChange={onChange}
+              />
+            </div>
+            <button className="btn btn-success" type="submit">
+              Add Person
+            </button>
+          </form>
+        </div>
+        <div className="col">
+          <h2>People:</h2>
+          <hr />
+          {people.map((p) => (
+            <div key={Math.random() * 100000000}>
+              <p>
+                {p.firstName} {p.lastName}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
